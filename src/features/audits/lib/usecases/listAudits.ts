f1628de5758bb2d2fe4b156f1951a7c8038ee769
Franco Ghilardi <franco.ghilardi1@gmail.com>
2025-11-04 14:20:34 -0300

    Se consume api de audits

---
+++ src/features/audits/lib/usecases/listAudits.ts
@@ -0,0 +1,23 @@
+import type { Audit, AuditType } from "@entities/audit/model";
+import type { AuditRepo, AuditListQuery } from "@entities/audit/api/audit.repo";
+import auditRepoImpl from "@features/audits/api/audit.repo.impl";
+
+export type ListAuditsInput = AuditListQuery;
+
+type Deps = {
+  repo: AuditRepo;
+};
+
+/**
+ * listAudits
+ * Caso de uso que abstrae el origen de datos (repo) y retorna Audits de dominio.
+ */
+export async function listAudits(deps?: Partial<Deps>): Promise<AuditType> {
+  const repo = deps?.repo ?? auditRepoImpl;
+
+  const audits = await repo.list();
+
+  return audits;
+}
+
+export default listAudits;
